785f0712cbce28d2ee3b7e72cda7b0b2363f1d12
Samandar Turdiev <firsturdiev@gmail.com>
2021-06-07 13:28:57 +0500

    Adding JavaScript interactions...

--- bookmark/js/main.js
+++ bookmark/js/main.js
@@ -3,13 +3,27 @@
 let elsTab = document.querySelectorAll(".tab");
 let elsTabBtn = document.querySelectorAll(".tab__control");
 
+let elsPanelItem = document.querySelectorAll(".panel__item");
+
 elsTabBtn.forEach((btn) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // tab
+
     elsTab.forEach((tab) => {
       tab.classList.remove("tab--active");
     });
 
     btn.parentElement.classList.add("tab--active");
+
+    // panel
+
+    elsPanelItem.forEach((panel) => {
+      panel.classList.remove("panel__item--active");
+    });
+
+    document.querySelector(btn.getAttribute('href')).classList.add('panel__item--active')
   });
 });
 
@@ -24,6 +38,6 @@ elsAccordionControl.forEach((btn) => {
       accordion.classList.remove("accordion__item--active");
     });
 
-    btn.closest('li').classList.add('accordion__item--active');
+    btn.closest("li").classList.add("accordion__item--active");
   });
 });
