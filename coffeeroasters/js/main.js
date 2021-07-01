7d376752177230f0f87e2b6fdf0e9a7ad85af3db
Samandar Turdiev <firsturdiev@gmail.com>
2021-07-01 19:00:39 +0500

    Adding jaavscript

--- coffeeroasters/js/main.js
+++ coffeeroasters/js/main.js
@@ -11,8 +11,29 @@ elMenuToggle.addEventListener("click", function () {
 
 let elsOptionToggle = document.querySelectorAll(".option__toggle");
 
-elsOptionToggle.forEach(toggle => {
-  toggle.addEventListener('click', () => {
-    toggle.closest('.option').classList.toggle('option--open')
-  })
-})
+elsOptionToggle.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.closest(".option").classList.toggle("option--open");
+  });
+});
+
+// modal
+
+let elModalOpen = document.querySelector(".order__btn");
+let elModal = document.querySelector(".subscribe__modal");
+let elModalInner = document.querySelector(".modal__inner");
+
+elModalOpen.addEventListener("click", (e) => {
+  e.preventDefault();
+  elModal.classList.add("modal--open");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === elModal) {
+    elModal.classList.remove("modal--open");
+  }
+});
+
+if (elModal.classList.contains("modal--open")) {
+  window.alert("wrcjc");
+}
