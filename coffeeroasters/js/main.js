38d54164dc0d59ee86c577291f4ac1e0b01e16c9
Samandar Turdiev <firsturdiev@gmail.com>
2021-07-01 16:32:10 +0500

    Adding subscribe section

--- coffeeroasters/js/main.js
+++ coffeeroasters/js/main.js
@@ -1,6 +1,18 @@
+//  menu
+
 const elMenuToggle = document.querySelector(".menu");
 const elMenu = document.querySelector(".sitenav");
 
 elMenuToggle.addEventListener("click", function () {
   elMenu.classList.toggle("sitenav--open");
 });
+
+// accordion
+
+let elsOptionToggle = document.querySelectorAll(".option__toggle");
+
+elsOptionToggle.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.closest('.option').classList.toggle('option--open')
+  })
+})
